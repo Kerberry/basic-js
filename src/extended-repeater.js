9b23bafd9fb12e68cac9d123e88039525b9e29e5
kerberry <trial404@mail.ru>
2022-05-02 12:56:56 +0300

    feat: and several tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,8 +16,49 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 function repeater(str, options) {
-  let result = str.toString();
-  options
+  let result = '';
+  if(options.repeatTimes){
+    for(let i = 0; i < options.repeatTimes; i++){
+      result += str.toString();
+      for(let j=0; j < options.additionRepeatTimes; j++ ){
+        if(options.addition){
+          result += options.addition.toString();
+        }
+        if(j < options.additionRepeatTimes -1){
+          if(options.additionSeparator){
+            result += options.additionSeparator;
+          }
+        }
+        
+      }
+      if(i < options.repeatTimes -1){
+        if(options.separator){
+          result += options.separator;
+        }
+        else{
+          result += '+';
+        }
+      }
+    }
+  }
+  else{
+    result += str.toString();
+    if(options.additionRepeatTimes){
+      for(let j=0; j < options.additionRepeatTimes; j++ ){
+        if(options.addition){
+          result += options.addition.toString();
+        }
+        if(options.additionSeparator){
+          result += options.additionSeparator;
+        }
+      }
+      
+    }
+    else{
+      result+=options.addition;
+    }
+  }
+  return result;
 }
 
 module.exports = {
